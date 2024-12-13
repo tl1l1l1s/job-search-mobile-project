5e3876e2513f941377bd348255cfc516c4a55ab1
tl1l1l1s <shj721553@gmail.com>
2024-12-13 19:11:34 +0900

    Feature: add map fragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,6 @@ dependencies {
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
     implementation ("androidx.lifecycle:lifecycle-common-java8:$lifecycle_version")
 
+    // GoogleMap
+    implementation ("com.google.android.gms:play-services-maps:19.0.0")
 }
